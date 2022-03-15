86b5a9c0d67ee6fd8e2982dcfcdca7c9fb79663d
Kofrán Dániel <63071083+phatslug@users.noreply.github.com>
2022-03-15 20:12:22 +0100

    proba 5

--- talan_megoldas/preprocess.py
+++ talan_megoldas/preprocess.py
@@ -1,5 +1,18 @@
 import pandas as pd
-
+import subprocess
+import requests
+import time
 
 if __name__ == "__main__":
     pd.read_csv("data.csv").to_pickle("data.pkl")
+
+proc = subprocess.Popen(["python", "main.py"])
+
+while True:
+    try:
+        time.sleep(1)
+        resp = requests.get("http://localhost:5555/started")
+        time.sleep(4)
+        break
+    except Exception as e:
+        pass
