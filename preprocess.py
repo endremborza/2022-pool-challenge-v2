6b171f17790e716d66ee5755c012d5c1d5e46f09
lilibenedikta <55950892+lilibenedikta@users.noreply.github.com>
2022-03-15 21:51:29 +0000

    lili_sára_gyenes
    hiányzott pár dolog

--- preprocess.py
+++ preprocess.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import pickle
 
-data = pd.read_csv("data.csv")
+if __name__ == "__main__":
+    data = pd.read_csv("data.csv")
 
-data["msec"] = data["msec"].astype(np.int32)
-data["subject"] = data["subject"].astype("category")
+    data["msec"] = data["msec"].astype(np.int32)
+    data["subject"] = data["subject"].astype("category")
 
-cols_1 = ['msec', 'x_position', 'y_position', 'z_position', 'subject','trial']
+    cols_1 = ['msec', 'x_position', 'y_position', 'z_position', 'subject','trial']
 
-data = data.loc[:,cols_1]
+    data = data.loc[:,cols_1]
 
-data.to_pickle("data.pkl")
+    data.to_pickle("data.pkl")
