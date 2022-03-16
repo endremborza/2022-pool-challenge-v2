54bf378f24ad943fbbda005f516a9c8fd793c848
Kofrán Dániel <63071083+phatslug@users.noreply.github.com>
2022-03-16 02:40:21 +0100

    hogy fut az api 4

--- basic_plus_api/simple_main.py
+++ basic_plus_api/simple_main.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
 out_cols = ["msec", "subject", "trial"]
 
-if __name__ == "__main__":
-    pd.read_csv("data.csv").to_pickle("data.pkl")
+pd.read_csv("data.csv").to_pickle("data.pkl")
 
 @app.route("/started")
 def started():
@@ -20,23 +19,21 @@ def started():
 
 @app.route("/")
 def solution():
-    if __name__ == "__main__":
-        df = pd.read_pickle("data.pkl")
-        input_locations = json.loads(Path("input.json").read_text())
-        out = []
-        for query in input_locations:
-            filt = (
-                (df["subject"] == query["subject"])
-                & (df["msec"] >= query["min_msec"])
-                & (df["msec"] <= query["max_msec"])
-            )
-            pos_arr = np.array([query[c] for c in pos_cols], ndmin=2)
-            min_ind = ((df.loc[filt, pos_cols] - pos_arr) ** 2).sum(axis=1).idxmin()
-            out.append(df.loc[min_ind, out_cols].to_dict())
+    df = pd.read_pickle("data.pkl")
+    input_locations = json.loads(Path("input.json").read_text())
+    out = []
+    for query in input_locations:
+        filt = (
+            (df["subject"] == query["subject"])
+            & (df["msec"] >= query["min_msec"])
+            & (df["msec"] <= query["max_msec"])
+        )
+        pos_arr = np.array([query[c] for c in pos_cols], ndmin=2)
+        min_ind = ((df.loc[filt, pos_cols] - pos_arr) ** 2).sum(axis=1).idxmin()
+        out.append(df.loc[min_ind, out_cols].to_dict())
 
-        Path("output.json").write_text(json.dumps(out))
+    Path("output.json").write_text(json.dumps(out))
     return "FING"
-
 
 app.dfo = (
     pd.read_csv("data.csv")
