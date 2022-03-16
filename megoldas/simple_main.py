8f42759b96ea1fb723ea8e723824552f8af3b0dc
arturvelk <64686836+arturvelk@users.noreply.github.com>
2022-03-16 16:26:51 +0100

    gyorsabb basic

--- megoldas/simple_main.py
+++ megoldas/simple_main.py
@@ -1,28 +1,36 @@
-import json
 import pandas as pd
 import numpy as np
+import json
 from pathlib import Path
-import scipy.spatial.distance
 
 pos_cols = [f"{ax}_position" for ax in ["x", "y", "z"]]
-out_cols = ["msec", "subject", "trial"]
+res_cols = ["msec", "subject", "trial"]
+
 
 if __name__ == "__main__":
-
     df = pd.read_pickle("data.pkl")
     input_locations = json.loads(Path("input.json").read_text())
-    df.drop_duplicates(subset=out_cols).drop_duplicates(subset=pos_cols)
-    input = np.array([list(i.values())[:3] for i in input_locations])
-    lista = []
-    for input_row in input_locations:
-        df_ = df[
-            (df["subject"] == input_row["subject"])
-            & (df["msec"] >= input_row["min_msec"])
-            & (df["msec"] <= input_row["max_msec"])
-        ]
-        input = list(input_row.values())[:3]
-        df_t = df_.reset_index().loc[:, ["x_position", "y_position", "z_position"]]
-        d = scipy.spatial.distance.cdist(list(df_t.values), np.array([input]))
-        shortest_index = d.argmin(axis=0)
-        lista.append(df_.iloc[shortest_index[0], [0, -2, -1]].to_dict())
-    Path("output.json").write_text(json.dumps(lista))
+    numpy_df = df.loc[:, pos_cols + res_cols].to_numpy()
+    numpy_input = pd.DataFrame(input_locations).to_numpy()
+
+    def solution_list(numpy_df, numpy_input):
+        out = []
+        for i in range(len(numpy_input)):
+            mask = (
+                (numpy_df[:, [-3]] >= numpy_input[:, [-2]][i][0])
+                & (numpy_df[:, [-3]] <= numpy_input[:, [-1]][i][0])
+                & (numpy_df[:, [-2]] == numpy_input[:, [-3]][i][0])
+            )
+            pos_arr = np.array([numpy_input[0][c] for c in [0, 1, 2]], ndmin=2)
+            dumma = (
+                (numpy_df[:, [0]][mask] - numpy_input[:, [0]][i]) ** 2
+                + (numpy_df[:, [1]][mask] - numpy_input[:, [1]][i]) ** 2
+                + (numpy_df[:, [2]][mask] - numpy_input[:, [2]][i]) ** 2
+            )
+            msec = numpy_df[:, [-3]][mask][np.where(dumma == np.amin(dumma))[0][0]]
+            subject = numpy_df[:, [-2]][mask][np.where(dumma == np.amin(dumma))[0][0]]
+            trial = numpy_df[:, [-1]][mask][np.where(dumma == np.amin(dumma))[0][0]]
+            out.append({"msec": msec, "subject": subject, "trial": trial})
+        return out
+
+    Path("output.json").write_text(json.dumps(solution_list(numpy_df, numpy_input)))
